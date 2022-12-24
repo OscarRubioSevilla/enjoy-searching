05b1166f40dc198037822a80b64b0b410078f697
OscarRubioSevilla <rubio.sevilla.oscar@gmail.com>
2022-12-23 21:52:56 -0600

    deploy

---
+++ precache-manifest.244bc4ceb542b02cb0825d4957fe149a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f549bf1585211109793",
+    "url": "/enjoy-searching/css/app.9e981aa5.css"
+  },
+  {
+    "revision": "584cf56adfa32ce4e9ed",
+    "url": "/enjoy-searching/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "54ea37fd885e7bf2acd7",
+    "url": "/enjoy-searching/css/customers.5edc9ccd.css"
+  },
+  {
+    "revision": "d096c0d511574bb41a18ed61deb5a06e",
+    "url": "/enjoy-searching/img/flag_lang_cn.d096c0d5.png"
+  },
+  {
+    "revision": "209843a904a0a0561fc8f9d0a7d69f66",
+    "url": "/enjoy-searching/img/flag_lang_en.209843a9.png"
+  },
+  {
+    "revision": "842830c634c0f42c35eca38d13f676d9",
+    "url": "/enjoy-searching/img/flag_lang_es.842830c6.png"
+  },
+  {
+    "revision": "33e0b1c0c12c6f759cb44f74941bf9cc",
+    "url": "/enjoy-searching/img/search_no_result.33e0b1c0.png"
+  },
+  {
+    "revision": "7dc91d65b7257cbcc8cde8b70c4e01dc",
+    "url": "/enjoy-searching/img/searching.7dc91d65.png"
+  },
+  {
+    "revision": "c3349addcff3d10fb3284f6882d2238e",
+    "url": "/enjoy-searching/img/spinner.c3349add.gif"
+  },
+  {
+    "revision": "6f3fe77a1869102fb34fc986dd2d2dc5",
+    "url": "/enjoy-searching/index.html"
+  },
+  {
+    "revision": "6f549bf1585211109793",
+    "url": "/enjoy-searching/js/app.60946246.js"
+  },
+  {
+    "revision": "584cf56adfa32ce4e9ed",
+    "url": "/enjoy-searching/js/chunk-vendors.b0910237.js"
+  },
+  {
+    "revision": "54ea37fd885e7bf2acd7",
+    "url": "/enjoy-searching/js/customers.ae8b55ce.js"
+  },
+  {
+    "revision": "b8f0186a080bb62558af3536f944b3e7",
+    "url": "/enjoy-searching/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/enjoy-searching/robots.txt"
+  }
+]);
